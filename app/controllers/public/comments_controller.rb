ddd8939355456f811744a842fe356892113b3d9b
mioreo6 <102738892+mioreo6@users.noreply.github.com>
2022-08-10 11:16:43 +0000

    [controllers]

---
+++ app/controllers/public/comments_controller.rb
@@ -0,0 +1,13 @@
+class Public::CommentsController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,47 @@
 Rails.application.routes.draw do
 
 
+  namespace :admin do
+    get 'favorites/index'
+  end
+  namespace :admin do
+    get 'customers/index'
+    get 'customers/show'
+    get 'customers/edit'
+  end
+  namespace :admin do
+    get 'comments/index'
+    get 'comments/show'
+  end
+  namespace :admin do
+    get 'posts/index'
+    get 'posts/show'
+  end
+  namespace :admin do
+    get 'homes/top'
+  end
+  namespace :public do
+    get 'comments/index'
+    get 'comments/new'
+    get 'comments/create'
+    get 'comments/show'
+  end
+  namespace :public do
+    get 'posts/index'
+    get 'posts/new'
+    get 'posts/create'
+    get 'posts/show'
+    get 'posts/edit'
+    get 'posts/destroy'
+  end
+  namespace :public do
+    get 'customers/show'
+    get 'customers/edit'
+  end
+  namespace :public do
+    get 'homes/top'
+    get 'homes/about'
+  end
 devise_for :customers,skip: [:passwords], controllers: {
   registrations: "public/registrations",
   sessions: 'public/sessions'
