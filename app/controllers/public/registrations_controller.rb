5c721a4a0a40db650ebdb42f9223fbdd18454a54
mioreo6 <102738892+mioreo6@users.noreply.github.com>
2022-08-13 10:47:23 +0000

    [complete]sign_in up

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -57,6 +57,8 @@ class Public::RegistrationsController < Devise::RegistrationsController
 
   # The path used after sign up for inactive accounts.
   # def after_inactive_sign_up_path_for(resource)
-  #   super(resource)
   # end
+  def after_sign_up_path_for(resource)
+     root_path
+  end
 end
