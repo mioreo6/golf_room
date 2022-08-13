6a408792260878afb5313e454f7ac2b79bed72c8
mioreo6 <102738892+mioreo6@users.noreply.github.com>
2022-08-13 11:40:15 +0000

    [admin]customers-index

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,10 +1,18 @@
 class Admin::CustomersController < ApplicationController
+  before_action :authenticate_admin!
   def index
+    @customers = Customer.page(params[:page]).per(20)
   end
 
   def show
   end
 
   def edit
+  end
+
+
+  private
+  def customer_params
+    params.require(:customer).permit(:id, :name, :email, :is_deleted, :introduction, :created_at)
   end
 end
