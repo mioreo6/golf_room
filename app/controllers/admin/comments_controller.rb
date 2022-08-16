d3d8cd1b6ac81e61fa025ea8a8fab9c13f39d240
mioreo6 <102738892+mioreo6@users.noreply.github.com>
2022-08-16 14:25:37 +0000

    [admin]comment

--- app/controllers/admin/comments_controller.rb
+++ app/controllers/admin/comments_controller.rb
@@ -2,11 +2,19 @@ class Admin::CommentsController < ApplicationController
   def index
     @comments = Comment.all
     @comment = Comment.find(params[:id])
-    
+
   end
 
   def show
+    @comment = Comment.find(params[:id])
   end
+  
+  def destroy
+    comment = Comment.find(params[:id])
+    comment.destroy
+    redirect_to admin_post_path(post.id)
+  end
+  
   private
   def comment_params
      params.require(:comment).permit(:id, :comment, :created_at, :customer_id)
