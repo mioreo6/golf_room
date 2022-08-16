e35de94884b8e491072c7d6aae1495342ac3ac68
mioreo6 <102738892+mioreo6@users.noreply.github.com>
2022-08-16 14:16:11 +0000

    [admin]comment index

--- app/controllers/admin/comments_controller.rb
+++ app/controllers/admin/comments_controller.rb
@@ -1,7 +1,14 @@
 class Admin::CommentsController < ApplicationController
   def index
+    @comments = Comment.all
+    @comment = Comment.find(params[:id])
+    
   end
 
   def show
+  end
+  private
+  def comment_params
+     params.require(:comment).permit(:id, :comment, :created_at, :customer_id)
   end
 end
