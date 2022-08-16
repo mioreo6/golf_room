e195f115f4fe51a9f8d691d043a138fe08861efd
mioreo6 <102738892+mioreo6@users.noreply.github.com>
2022-08-16 13:46:27 +0000

    [admin]post show

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -1,7 +1,13 @@
 class Admin::PostsController < ApplicationController
   def index
+    @posts = Post.page(params[:page]).per(20)
   end
 
   def show
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:id, :customer_id, :created_at, :body)
   end
 end
