b730478b03722a2713a34625b919a81edfb1b264
mioreo6 <102738892+mioreo6@users.noreply.github.com>
2022-08-22 13:07:07 +0000

    [update]public post edit

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -1,6 +1,7 @@
 class Public::PostsController < ApplicationController
   def index
     @posts = Post.all
+
   end
 
   def new
@@ -10,7 +11,7 @@ class Public::PostsController < ApplicationController
   def create
     post = Post.new(post_params)
     post.customer_id = current_customer.id
-    
+
     post.save
     redirect_to root_path
   end
@@ -20,9 +21,19 @@ class Public::PostsController < ApplicationController
   end
 
   def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    redirect_to post_path(@post)
   end
 
   def destroy
+    post = Post.find(params[:id])
+    post.destroy
+    redirect_to posts_path
   end
 
   private
