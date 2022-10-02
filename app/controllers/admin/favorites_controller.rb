160a7b9ba3d02f5438efb789857cf76fc1a16615
mioreo6 <msmiiiiii3636@gmail.com>
2022-10-02 15:09:37 +0000

    [update]admin-favorite all

--- app/controllers/admin/favorites_controller.rb
+++ app/controllers/admin/favorites_controller.rb
@@ -1,4 +1,9 @@
 class Admin::FavoritesController < ApplicationController
   def index
   end
+
+  def all
+    @customer = Customer.find(params[:customer_id])
+    @favorites = @customer.favorites
+  end
 end
